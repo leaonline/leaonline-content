c4afcd12692e2826331427794878a718ae766103
Jan Küster <jkuester@uni-bremen.de>
2019-10-10 12:39:31 +0200

    add Log controller

--- imports/startup/server/tts.js
+++ imports/startup/server/tts.js
@@ -2,6 +2,7 @@ import { WebApp } from 'meteor/webapp'
 import { TTSFiles } from '../../api/tts/TTSFiles'
 import { TTSBackend } from '../../api/tts/TTSEngine'
 import { createFilesCollection } from '../../utils/collection'
+import { Log } from '../../utils/log'
 
 const TTSFilesCollection = createFilesCollection(TTSFiles)
 
@@ -11,17 +12,18 @@ WebApp.connectHandlers.use('/tts', Meteor.bindEnvironment(function (req, res, ne
 
   try {
     const { text } = req.body
-    console.log(req.method, req.body, req.query)
+    Log.debug(req.method, req.body, req.query)
     if (!text || text.length === 0) {
       res.writeHead(404)
       return res.end()
     }
     const filesDoc = TTSBackend.get({ text })
     const url = TTSFilesCollection.link(filesDoc)
-    res.setHeader('Content-Type', 'application/json')
+    Log.debug(url)
+    res.setHeader('Content-Type', 'application/json;UTF-8')
     res.end(JSON.stringify({ url }))
   } catch (error) {
-    console.error(error)
+    Log.error(error)
     res.writeHead(500)
     res.end('internal server error during request')
   }
---
+++ imports/utils/log.js
@@ -0,0 +1,14 @@
+export const Log = {}
+
+Log.debug = function (...args) {
+  if (Meteor.isDevelopment) {
+    console.log(...args)
+  }
+}
+
+Log.error = function (...args) {
+  if (Meteor.isDevelopment) {
+    console.error(...args)
+  }
+  // TODO add to error log
+}
