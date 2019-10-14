5a84a7bb095ba8a90b44b85dd0393539c7fcdae9
Jan Küster <jkuester@uni-bremen.de>
2019-10-14 18:09:59 +0200

    use leaonline:ddp-login-hanlder to configre remote login validatioN

--- imports/startup/server/accounts.js
+++ imports/startup/server/accounts.js
@@ -1,11 +1,5 @@
 import { Meteor } from 'meteor/meteor'
-import { OAuth } from 'meteor/oauth'
-import { HTTP } from 'meteor/http'
-
-let userAgent = 'Meteor'
-if (Meteor.release) {
-  userAgent += `/${Meteor.release}`
-}
+import { registerOAuthDDPLoginHandler } from 'meteor/leaonline:ddp-login-handler'
 
 Meteor.startup(() => {
   const { oauth } = Meteor.settings
@@ -24,43 +18,5 @@ Meteor.startup(() => {
     }
   )
 
-  Accounts.registerLoginHandler('loginWithLea', function (options) {
-    if (!options || !options.lea) return
-
-    const { accessToken } = options
-
-    let response
-    const requestOptions = {
-      headers: { Accept: 'application/json', 'User-Agent': userAgent, Authorization: `Bearer ${accessToken}` },
-    }
-
-    try {
-      response = HTTP.get(oauth.identityUrl, requestOptions)
-    } catch (err) {
-      throw new Error(`Failed to fetch identity from lea. ${err.message}`), { response: err.response }
-    }
-
-    const { data } = response
-    if (!data.id || !data.login) {
-      throw new Error('unecpected data result')
-    }
-
-    let userDoc = Meteor.users.findOne({ 'services.lea.id': data.id })
-    if (!userDoc) {
-      const userId = Meteor.users.insert({
-        createdAt: new Date(),
-        services: {
-          lea: {
-            id: data.id,
-            accessToken: accessToken,
-            username: data.login
-          }
-        }
-      })
-
-      userDoc = Meteor.users.findOne(userId)
-    }
-
-    return {userId: userDoc._id}
-  })
+  registerOAuthDDPLoginHandler({ identityUrl: oauth.identityUrl })
 })
