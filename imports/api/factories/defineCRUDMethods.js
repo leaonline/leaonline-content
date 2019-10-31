79676b81e76c8edb88b02d3e79ca8c028e433094
jankapunkt <jkuester@uni-bremen.de>
2019-10-31 15:23:00 +0100

    api factories add define methods

---
+++ imports/api/factories/defineCRUDMethods.js
@@ -0,0 +1,51 @@
+import { onServer } from '../../utils/arch'
+import { getCollection } from '../../utils/collection'
+
+export const defineUpdateMethod = ({ name, schema, roles, group, isPublic, timeInterval, numRequests, run }) => {
+  const runFct = run || (function (updateDoc) {
+    const Collection = getCollection(name)
+    if (!Collection) throw new Error(`Expected collection by name <${name}>`)
+
+    const { _id } = updateDoc
+    const document = Collection.findOne(_id)
+    if (!document) {
+      return Collection.insert(updateDoc)
+    } else {
+      delete document._id
+      return Collection.update(_id, { $set: document })
+    }
+  })
+
+  return {
+    name: `${name}.methods.update`,
+    schema: Object.assign({}, schema, {
+      _id: {
+        type: String,
+        optional: true
+      }
+    }),
+    isPublic: isPublic || true, // FIXME
+    numRequests: numRequests || 1,
+    timeInterval: timeInterval || 250,
+    run: onServer(runFct)
+  }
+}
+
+export const defineRemoveMethod = ({ name, isPublic, roles, group, timeInterval, numRequests, run }) => {
+  const runFct = run || (function ({ _id }) {
+    const Collection = getCollection(name)
+    if (!Collection) throw new Error(`Expected collection by name <${name}>`)
+    return Collection.remove(_id)
+  })
+  return {
+    name: `${name}.methods.remove`,
+    schema: {
+      _id: String
+    },
+    isPublic: isPublic || true, // FIXME
+    numRequests: numRequests || 1,
+    timeInterval: timeInterval || 1000,
+    run: onServer(runFct)
+  }
+
+}
