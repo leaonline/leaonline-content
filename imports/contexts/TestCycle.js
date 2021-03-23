c290e77f3d37e2bc0760598c434661b34831eea1
Jan Küster <jkuester@uni-bremen.de>
2021-03-23 14:34:36 +0100

    contexts TestCycle added

--- imports/contexts/TestCycle.js
+++ imports/contexts/TestCycle.js
@@ -1,1 +1,29 @@
-export { TestCycle } from 'meteor/leaonline:corelib/contexts/TestCycle'
+import { TestCycle } from 'meteor/leaonline:corelib/contexts/TestCycle'
+
+TestCycle.routes.all.run = function () {
+  const api = this
+  const { field, isLegacy } = api.data()
+  const query = {}
+
+  if (isLegacy === 'true') {
+    query.isLegacy = true
+  }
+
+  if (field) {
+    query.field = field
+  }
+
+  if (Object.keys(query).length === 0) {
+    this.error({
+      code: 500,
+      title: 'routes.queryFailed',
+      description: 'routes.emptyQueryNotSupported',
+      info: JSON.stringify({ query })
+    })
+  }
+
+  api.log(query, TestCycle.collection().find(query).count())
+  return TestCycle.collection().find(query).fetch()
+}
+
+export { TestCycle }
