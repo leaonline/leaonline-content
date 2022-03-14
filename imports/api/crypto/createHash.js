212202fc12147ea29a03211833b09e5b1a990dc4
Jan Küster <jkuester@uni-bremen.de>
2022-03-14 12:36:34 +0100

    removed: unused files deleted

--- imports/api/crypto/createHash.js
+++
@@ -1,9 +0,0 @@
-import { Meteor } from 'meteor/meteor'
-import crypto from 'crypto'
-
-const { hashing } = Meteor.settings
-
-export const createHash = secret => crypto
-  .createHash(hashing.algorithm)
-  .update(secret)
-  .digest(hashing.digest)
--- imports/api/crypto/tokenIsValid.js
+++
@@ -1,7 +0,0 @@
-import { tokens } from './tokens'
-
-export const tokenIsValid = ({ client, token }) => {
-  const data = tokens.get(client)
-
-  return data && data.token === token && Date.now() < token.expires
-}
