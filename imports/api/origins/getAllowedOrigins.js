3f8c739e9762c29e9d52ab044f1c5926df0bb899
jankapunkt <jkuester@uni-bremen.de>
2023-07-12 14:01:06 +0200

    fix: standard js lint fix

--- imports/api/origins/getAllowedOrigins.js
+++ imports/api/origins/getAllowedOrigins.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor'
 
 const urls = Object.values(Meteor.settings.hosts).map(host => host.url)
+// eslint-disable-next-line
 const regExp = new RegExp(urls.map(url => url.replace('//', '\/\/')).join('|'), 'i')
 
 export const getAllowedOrigins = () => {
